2972447cada68d25481680ed72258f03935d4814
IanAvi15 <icaviles20@hotmail.com>
2026-02-15 19:47:52 -0400

    adding code

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char str[] = "_putchar";
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+	return (0);
+}
